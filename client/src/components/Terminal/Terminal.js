5cfa42f6706e9d7a54238335508769656f7c07f2
mekaknepley <mekaknepley@gmail.com>
2017-10-23 12:03:12 -0400

    add ons to the terminal page

--- client/src/components/Terminal/Terminal.js
+++ client/src/components/Terminal/Terminal.js
@@ -22,6 +22,18 @@ class Terminal extends React.Component {
                         <div className="panel-top">
 
                             <div id="terminalBody">
+                                <nav className="sideNav blue-grey darken-4">
+                                    <a href="#" data-activates="slide-out" className="button-collapse show-on-large"><i class="small material-icons center"                     id="icons">apps</i></a>
+                                    <br/>
+                                        <i className="small material-icons center" id="icons">chat</i>
+                                        <i className="small material-icons center" id="icons">voice_chat</i>
+                                        <i className="small material-icons center" id="icons">person_add</i>
+                                        <i className="small material-icons center" id="icons">directions_run</i>
+                                        <a id="logo-container " href="#" class="brand-logo"></a>
+                                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                                            <li><a href="#" id="isSignDiv"></a></li>
+                                        </ul>
+                                </nav>
                                 <form id="preview-form">
                                     <textarea className="codemirror-textarea" name="editor-value" id="editor-value" rows="4" cols="10">&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;&#13;&#10;</textarea>
                                     <br/>
