0e1988bca1064cca4b71beb429a0c5e7a5116993
mekaknepley <mekaknepley@gmail.com>
2017-10-28 18:14:04 -0400

    setup

--- client/src/components/Terminal/Terminal.js
+++ client/src/components/Terminal/Terminal.js
@@ -184,14 +184,14 @@ class Terminal extends Component {
                                 {/*<nav className="sideNav blue-grey darken-4">*/}
                                     {/*<a href="#" data-activates="slide-out" className="button-collapse show-on-large"><i class="small material-icons center" id="icons">apps</i></a>*/}
                                     {/*<br/>*/}
-                                        {/*<i className="small material-icons center" id="icons">chat</i>*/}
-                                        {/*<i className="small material-icons center" id="icons">voice_chat</i>*/}
-                                        {/*<i className="small material-icons center" id="icons">person_add</i>*/}
-                                        {/*<i className="small material-icons center" id="icons">directions_run</i>*/}
-                                        {/*<a id="logo-container " href="#" class="brand-logo"></a>*/}
-                                        {/*<ul id="nav-mobile" class="right hide-on-med-and-down">*/}
-                                            {/*<li><a href="#" id="isSignDiv"></a></li>*/}
-                                        {/*</ul>*/}
+                                        <i className="small material-icons center" id="icons">chat</i>
+                                        <i className="small material-icons center" id="icons">voice_chat</i>
+                                        <i className="small material-icons center" id="icons">person_add</i>
+                                        <i className="small material-icons center" id="icons">directions_run</i>
+                                        <a id="logo-container " href="#" class="brand-logo"></a>
+                                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                                            <li><a href="#" id="isSignDiv"></a></li>
+                                        </ul>
                                     {/*<SideNav className="sideNav"*/}
                                              {/*trigger={<a href="#" data-activates="slide-out" className="button-collapse show-on-large"><i class="small material-icons center" id="icons">apps</i></a>}*/}
                                              {/*options={{ closeOnClick: true }}*/}
